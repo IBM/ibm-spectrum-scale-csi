b84bd952b936d8d4c532501cf970df533b718c65
Dimitrios Skourtis <Dimitrios.Skourtis@ibm.com>
2019-04-04 14:45:17 -0700

    Fixed destroy.sh paths

--- deploy/containerized/destroy.sh
+++ deploy/containerized/destroy.sh
@@ -1,10 +1,10 @@
-kubectl delete -f deploy/csi-plugin.yaml
-kubectl delete -f deploy/csi-plugin-attacher.yaml
-kubectl delete -f deploy/csi-plugin-provisioner.yaml
+kubectl delete -f deploy/common/csi-plugin.yaml
+kubectl delete -f deploy/common/csi-plugin-attacher.yaml
+kubectl delete -f deploy/common/csi-plugin-provisioner.yaml
 
-kubectl delete -f deploy/csi-attacher-rbac.yaml
-kubectl delete -f deploy/csi-nodeplugin-rbac.yaml
-kubectl delete -f deploy/csi-provisioner-rbac.yaml
+kubectl delete -f deploy/common/csi-attacher-rbac.yaml
+kubectl delete -f deploy/common/csi-nodeplugin-rbac.yaml
+kubectl delete -f deploy/common/csi-provisioner-rbac.yaml
 
 kubectl delete configmap cmap-config
 
