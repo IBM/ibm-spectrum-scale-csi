65d537615f256652cf0e64086dd953a15bf4576b
John Dunham <jdunham@us.ibm.com>
2019-09-16 07:27:53 -0700

    Renamed crs and crds.

--- example/csi-scale-destroy.sh
+++ example/csi-scale-destroy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 kubectl delete csi-scale-operator --all
-kubectl delete -f deploy/crds/cache_v1alpha1_podset_cr.yaml
+kubectl delete -f deploy/crds/csi-scale-operators_v1alpha1_podset_cr.yaml
 kubectl delete -f deploy/operator.yaml
 kubectl delete -f deploy/role.yaml
 kubectl delete -f deploy/role_binding.yaml
 kubectl delete -f deploy/service_account.yaml
-kubectl delete -f deploy/crds/cache_v1alpha1_podset_crd.yaml
+kubectl delete -f deploy/crds/csi-scale-operators_v1alpha1_podset_crd.yaml
 
 kubectl delete -f deploy/spectrum_scale.yaml
 
