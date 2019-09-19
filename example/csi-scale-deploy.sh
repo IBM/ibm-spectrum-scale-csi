9207637b1399b9e40e6cbf3775ee98391a1dcc16
John Dunham <jdunham@us.ibm.com>
2019-09-19 06:00:12 -0700

    Reworking my example launchers. Now the Custom Resource is not created by default.

--- example/csi-scale-deploy.sh
+++ example/csi-scale-deploy.sh
@@ -20,8 +20,8 @@ kubectl create -f deploy/service_account.yaml
 kubectl create -f deploy/role.yaml
 kubectl create -f deploy/role_binding.yaml
 kubectl create -f deploy/crds/csi-scale-operators_v1alpha1_podset_crd.yaml
-kubectl create -f deploy/crds/csi-scale-operators_v1alpha1_podset_cr.yaml --save-config
-kubectl apply -f deploy/spectrum_scale.yaml
+#kubectl create -f deploy/crds/csi-scale-operators_v1alpha1_podset_cr.yaml --save-config
+#kubectl create -f deploy/spectrum_scale.yaml
 
 kubectl create -f deploy/operator.yaml
 
