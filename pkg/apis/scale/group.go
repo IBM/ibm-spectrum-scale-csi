4c479523bfec1752bda6f4ac01812e8edae9d126
John Dunham <jdunham@us.ibm.com>
2019-09-30 08:42:49 -0700

    Transitioning to v0.8.2 for operator sdk, because the v0.10.0 version couldn't build the go operator.

--- pkg/apis/scale/group.go
+++
@@ -1,6 +0,0 @@
-// Package scale contains scale API versions.
-//
-// This file ensures Go source parsers acknowledge the scale package
-// and any child packages. It can be removed if any other Go source files are
-// added to this package.
-package scale
--- pkg/apis/scale/v1alpha1/doc.go
+++
@@ -1,4 +0,0 @@
-// Package v1alpha1 contains API Schema definitions for the scale v1alpha1 API group
-// +k8s:deepcopy-gen=package,register
-// +groupName=scale.ibm.com
-package v1alpha1
