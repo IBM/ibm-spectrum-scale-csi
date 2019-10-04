8ca1b3dc7f064c43c324024316ec873de2e054c0
John Dunham <jdunham@us.ibm.com>
2019-10-04 11:32:15 -0700

    Wrote a script to handle spec descriptors and initialize new CRDs.

---
+++ stable/ibm-spectrum-scale-csi-operator-bundle/operators/ibm-spectrum-scale-csi-operator/hacks/copy_crd_descriptions.py
@@ -0,0 +1,108 @@
+#!/bin/python
+# TODO write a script to break up yaml into multiple files.
+
+import argparse
+import sys
+import os
+import yaml
+
+def main(args):
+    parser = argparse.ArgumentParser(
+        description='''A hack to clone descriptions from the CRD.''')
+    
+    parser.add_argument( '--crd', metavar='crd', dest='crd', default=None,
+        help='''The Custom Resource Definition File.''')
+
+    parser.add_argument( '--csv', metavar='csv', dest='csv', default=None,
+        help='''The output CSV to clone the data to.''')
+    parser.add_argument( '-p', metavar='prefix', dest='prefix', default='ibm-csi-scale',
+        help='''The prefix for the generated file names.''')
+
+    args = parser.parse_args()
+
+    if args.crd is None:
+        print("Missing Custom Resource Definition")
+        return 1
+    if args.csv is None:
+        print("Missing CSV")
+        return 1
+    
+    crdf=args.crd
+    if crdf[0] is not '/':
+        crdf ="{0}/{1}".format(os.getcwd(), crdf)
+
+    csvf=args.csv
+    if csvf[0] is not '/':
+        csvf ="{0}/{1}".format(os.getcwd(), csvf)
+
+    crd = None
+    csv = None
+    try:
+        with open(crdf, 'r') as stream:
+            crd = yaml.safe_load(stream)
+        with open(csvf, 'r') as stream:
+            csv = yaml.safe_load(stream)
+    except yaml.YAMLError as e:
+        print(e)
+        return 1
+
+    if crd is not None and csv is not None:
+        metaname= crd.get("metadata",{}).get("name", "")
+        props = crd.get("spec", {})    \
+            .get("validation",{})      \
+            .get("openAPIV3Schema", {})\
+            .get("properties", {})     \
+        
+        spec            = props.get("spec", {})
+        specprops       = spec.get("properties", {})
+        specdescriptors = []
+        specpaths       = specprops.keys()
+
+        while len(specpaths) > 0:
+            # Load the path and then iterate to get the property.
+            path    = specpaths.pop(0)
+            keys    = path.split(".")
+            prop    = spec
+            subprops = specprops
+          
+            for key in keys:
+                prop     = subprops.get(key, {})
+                subprops = prop.get("properties", {})
+            
+            # Enqueue the sub properties (if present)
+            for subprop in prop.get("properties", {}).keys():
+                specpaths.append("{0}.{1}".format(path,subprop))
+            
+            # Construct description
+            specdescriptors.append({ 
+              "path" : path,
+              "description" : prop.get("description", "") })
+
+        # TODO need to replace with something 
+        resourcefound=False
+        owned = csv.get("spec",{}).get("customresourcedefinitions",{}).get("owned",{})
+        for resource in owned:
+            if resource.get("name","") == metaname:
+                resourcefound = True
+                resource["specDescriptors"] = specdescriptors
+
+        # If the resource wasn't present in the customresourcedefinitions add it. 
+        if not resourcefound:
+            owned.append({
+                "name"            : metaname,
+                "kind"            : crd.get("kind","CustomResourceDefinition"),
+                "displayName"     : metaname,
+                "specDescriptors" : specdescriptors,
+                "description"     : "TODO: Fill this in"})
+
+
+        with open(csvf, 'w') as outfile:
+            yaml.dump(csv, outfile, default_flow_style=False)
+        
+
+
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
+
