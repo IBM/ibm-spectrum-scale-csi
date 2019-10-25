af0a97ad5c3ce6ad2e9f1697b914419c4e789d6d
John Dunham <jdunham@us.ibm.com>
2019-10-25 07:19:54 -0700

    Clearing validation warnings.

--- stable/ibm-spectrum-scale-csi-operator-bundle/operators/ibm-spectrum-scale-csi-operator/hacks/csi-scale-destroy.sh
+++ stable/ibm-spectrum-scale-csi-operator-bundle/operators/ibm-spectrum-scale-csi-operator/hacks/csi-scale-destroy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 kubectl delete csiscaleoperators --all
-kubectl delete -f deploy/spectrum_scale.yaml
+kubectl delete -f example/spectrum_scale.yaml
 kubectl delete -f deploy/operator.yaml
 kubectl delete -f deploy/role.yaml
 kubectl delete -f deploy/service_account.yaml
