d2e55a24ec6d2713d9e2ca6319bbd88ae13d055d
John Dunham <jdunham@us.ibm.com>
2019-11-14 13:25:18 -0800

    Working on getting the operator working in OLM with the namespacing code, had to change some service account names and retargeted the image to the new repos.

--- stable/ibm-spectrum-scale-csi-operator-bundle/operators/ibm-spectrum-scale-csi-operator/deploy/olm-scripts/operator-push.sh
+++ stable/ibm-spectrum-scale-csi-operator-bundle/operators/ibm-spectrum-scale-csi-operator/deploy/olm-scripts/operator-push.sh
@@ -6,7 +6,7 @@ cd ${DIR}/..
 export OPERATOR_DIR=olm-catalog/ibm-spectrum-scale-csi-operator
 export QUAY_NAMESPACE=mew2057
 export PACKAGE_NAME=ibm-spectrum-scale-csi-operator-app
-export PACKAGE_VERSION=0.1.5
+export PACKAGE_VERSION=0.1.12
 export TOKEN=$(curl -sH "Content-Type: application/json" -XPOST https://quay.io/cnr/api/v1/users/login -d '
 {"user": {"username": "'"${QUAY_USERNAME}"'","password": "'"${QUAY_PASSWORD}"'"}}' | cut -d'"' -f4)
 
