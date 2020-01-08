f7ecc778b6c8f7e00f3397caa1293c5f3e0660c4
John Dunham <jdunham@us.ibm.com>
2020-01-08 11:26:47 -0800

    Removed extraneous comment from package_operator.

--- stable/ibm-spectrum-scale-csi-operator-bundle/operators/ibm-spectrum-scale-csi-operator/hacks/package_operator.py
+++ stable/ibm-spectrum-scale-csi-operator-bundle/operators/ibm-spectrum-scale-csi-operator/hacks/package_operator.py
@@ -14,9 +14,6 @@ OPERATOR="{0}/ibm-spectrum-scale-csi-operator".format(OLM_CATALOG)
 
 PACKAGE_POSTFIX="package.yaml"
 
-#1.0.1
-
-
 def main( args ):
   parser = argparse.ArgumentParser(
       description='''
