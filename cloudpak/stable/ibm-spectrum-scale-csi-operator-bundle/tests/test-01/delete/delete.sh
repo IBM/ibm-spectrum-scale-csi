5cd94b468925c552d4e4b1dc09543dee12d66947
John Dunham <jdunham@us.ibm.com>
2020-02-17 13:07:51 -0800

    Moving additional references to the old cr/crd format to the new one.

--- cloudpak/stable/ibm-spectrum-scale-csi-operator-bundle/tests/test-01/delete/delete.sh
+++ cloudpak/stable/ibm-spectrum-scale-csi-operator-bundle/tests/test-01/delete/delete.sh
@@ -14,8 +14,8 @@ echo "deleteDir is "
 echo $deleteDir
 $APP_TEST_LIBRARY_FUNCTIONS/operatorDelete.sh \
     --serviceaccount $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/service_account.yaml \
-    --crd $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/crds/ibm-spectrum-scale-csi-operator-crd.yaml \
-    --cr $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/crds/ibm-spectrum-scale-csi-operator-cr.yaml \
+    --crd $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/crds/csiscaleoperators.csi.ibm.com.crd.yaml \
+    --cr $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/crds/csiscaleoperators.csi.ibm.com.cr.yaml \
     --role $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/role.yaml \
     --rolebinding $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/role_binding.yaml \
     --operator $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/operator.yaml \
