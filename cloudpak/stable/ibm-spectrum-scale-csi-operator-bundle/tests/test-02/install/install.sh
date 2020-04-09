6724b796ec49114e056bd4bd66d07a34d53f8600
John Dunham <jdunham@us.ibm.com>
2020-04-09 07:51:15 -0700

    Fixing  CR name in  tests.

--- cloudpak/stable/ibm-spectrum-scale-csi-operator-bundle/tests/test-02/install/install.sh
+++ cloudpak/stable/ibm-spectrum-scale-csi-operator-bundle/tests/test-02/install/install.sh
@@ -10,4 +10,4 @@ operator=ibm-spectrum-scale-csi-operator
 installDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 $APP_TEST_LIBRARY_FUNCTIONS/operatorInstall.sh \
-	--cr $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/crds/csiscaleoperators.csi.ibm.com.cr.yaml
+	--cr $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/crds/csiscaleoperators.csi.ibm.com_cr.yaml
