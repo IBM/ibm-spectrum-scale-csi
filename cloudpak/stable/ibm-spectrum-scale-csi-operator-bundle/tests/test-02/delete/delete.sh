87716c379e38a522870e1f6d20d2cbe2bf708f0b
John Dunham <jdunham@us.ibm.com>
2020-04-13 14:07:22 -0700

    Manually calling delete function in test.

--- cloudpak/stable/ibm-spectrum-scale-csi-operator-bundle/tests/test-02/delete/delete.sh
+++ cloudpak/stable/ibm-spectrum-scale-csi-operator-bundle/tests/test-02/delete/delete.sh
@@ -11,13 +11,19 @@ deleteDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "deleteDir is "
 echo $deleteDir
 
-$APP_TEST_LIBRARY_FUNCTIONS/operatorDelete.sh \
-    --serviceaccount $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/service_account.yaml \
-    --crd $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/crds/csiscaleoperators.csi.ibm.com.crd.yaml \
-    --cr $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/crds/csiscaleoperators.csi.ibm.com_cr.yaml \
-    --role $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/role.yaml \
-    --rolebinding $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/role_binding.yaml \
-    --operator $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/operator.yaml \
+kubectl delete -f $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/crds/csiscaleoperators.csi.ibm.com.crd.yaml
+kubectl delete -f $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/role_binding.yaml
+kubectl delete -f $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/role.yaml
+kubectl delete -f $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/service_account.yaml 
+kubectl delete namespace ${CV_TEST_NAMESPACE}
+
+#$APP_TEST_LIBRARY_FUNCTIONS/operatorDelete.sh \
+#    --serviceaccount $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/service_account.yaml \
+#    --crd $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/crds/csiscaleoperators.csi.ibm.com.crd.yaml \
+#    --cr $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/crds/csiscaleoperators.csi.ibm.com_cr.yaml \
+#    --role $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/role.yaml \
+#    --rolebinding $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/role_binding.yaml \
+#    --operator $CV_TEST_BUNDLE_DIR/operators/${operator}/deploy/operator.yaml \
 
 #deleteNamespace ${CV_TEST_NAMESPACE}
 
