7c637a7458a7667b551dc0d52e67924b811ac90d
Abhishek Jain <abjain39@in.ibm.com>
2021-01-27 21:28:17 +0530

    ignoring CPU parameters

--- tools/scripts/ci/install_minikube.sh
+++ tools/scripts/ci/install_minikube.sh
@@ -24,7 +24,7 @@ echo $HOME
 
 # Start minikube and chown for minikub.
 sudo chown -R travis: /home/travis/.minikube/
-sudo minikube start --driver=none --kubernetes-version=${KUBE_VERSION} --cpus=2
+sudo minikube start --driver=none --kubernetes-version=${KUBE_VERSION} --extra-config=kubeadm.ignore-preflight-errors=NumCPU --force
 minikube update-context
 eval $(minikube docker-env)
 
