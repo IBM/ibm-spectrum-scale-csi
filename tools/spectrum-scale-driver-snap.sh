50bebba049a6d9d5edd2d36d251db29804c595fc
Chetan R Kulkarni <chetkulk@in.ibm.com>
2021-07-09 05:25:06 -0700

    find out csi version and images (#425)

--- tools/spectrum-scale-driver-snap.sh
+++ tools/spectrum-scale-driver-snap.sh
@@ -17,11 +17,85 @@
 
 #USAGE spectrum-scale-driver-snap.sh [-n namespace] [-o output-dir] [-h]
 
+function find_versions()
+{
+
+  ns=$1
+  cmd=$2
+
+  operator_image="null"
+  csi_version="null"
+  driver_image="null"
+  registrar_image="null"
+  attacher_image="null"
+  provisioner_image="null"
+  snapshotter_image="null"
+
+  driver_pod=`$cmd -n $ns get pod -l app=ibm-spectrum-scale-csi -o jsonpath="{.items[0].metadata.name}"`
+  operator_pod=`$cmd -n $ns get pod -l name=ibm-spectrum-scale-csi-operator -o jsonpath="{.items[0].metadata.name}"`
+  attacher_pod=`$cmd -n $ns get pod -l app=ibm-spectrum-scale-csi-attacher -o jsonpath="{.items[0].metadata.name}"`
+  provisioner_pod=`$cmd -n $ns get pod -l app=ibm-spectrum-scale-csi-provisioner -o jsonpath="{.items[0].metadata.name}"`
+  snapshotter_pod=`$cmd -n $ns get pod -l app=ibm-spectrum-scale-csi-snapshotter -o jsonpath="{.items[0].metadata.name}"`
+
+  #get operator image
+  if [[ $operator_pod != ibm-spectrum-scale-csi-operator* ]]
+  then
+    echo "ibm-spectrum-scale-csi operator pod is not running in namespace $ns. Can't extract operator version."
+  else
+    operator_image=`$cmd -n $ns get pod $operator_pod -o jsonpath='{.status.containerStatuses[?(@.name=="operator")].image}'`
+  fi
+
+  #get driver image, registrar image and csi version from driver image
+  if [[ $driver_pod != ibm-spectrum-scale-csi-* ]]
+  then
+    echo "ibm-spectrum-scale-csi driver pod is not running in namespace $ns. Can't extract driver and registrar version."
+  else
+    driver_image=`$cmd -n $ns get pod $driver_pod -o jsonpath='{.status.containerStatuses[?(@.name=="ibm-spectrum-scale-csi")].image}'`
+    registrar_image=`$cmd -n $ns get pod $driver_pod -o jsonpath='{.status.containerStatuses[?(@.name=="driver-registrar")].image}'`
+    csi_version=`echo $driver_image | cut -f2 -d:`
+  fi
+
+  #get attacher image
+  if [[ $attacher_pod != ibm-spectrum-scale-csi-attacher* ]]
+  then
+    echo "ibm-spectrum-scale-csi attacher pod is not running in namespace $ns. Can't extract attacher version."
+  else
+    attacher_image=`$cmd -n $ns get pod $attacher_pod -o jsonpath='{.status.containerStatuses[?(@.name=="ibm-spectrum-scale-csi-attacher")].image}'`
+  fi
+
+  #get provisioner image
+  if [[ $provisioner_pod != ibm-spectrum-scale-csi-provisioner* ]]
+  then
+    echo "ibm-spectrum-scale-csi provisioner pod is not running in namespace $ns. Can't extract provisioner version."
+  else
+    provisioner_image=`$cmd -n $ns get pod $provisioner_pod -o jsonpath='{.status.containerStatuses[?(@.name=="csi-provisioner")].image}'`
+  fi
+
+  #get snapshotter image
+  if [[ $snapshotter_pod != ibm-spectrum-scale-csi-snapshotter* ]]
+  then
+    echo "ibm-spectrum-scale-csi snapshotter pod is not running in namespace $ns. Can't extract snapshotter version."
+  else
+    snapshotter_image=`$cmd -n $ns get pod $snapshotter_pod -o jsonpath='{.status.containerStatuses[?(@.name=="csi-snapshotter")].image}'`
+  fi
+
+  #print collected data
+  echo "IBM Spectrum Scale CSI driver : $csi_version"
+  echo "Operator Image                : $operator_image"
+  echo "Driver Image                  : $driver_image"
+  echo "Node Registrar Image          : $registrar_image"
+  echo "Attacher Image                : $attacher_image"
+  echo "Provisioner Image             : $provisioner_image"
+  echo "Snapshotter Image             : $snapshotter_image"
+
+}
+
 ns="ibm-spectrum-scale-csi-driver"
 outdir="."
 cmd="kubectl"
+version_flag=0
 
-while getopts 'n:o:h' OPTION; do
+while getopts 'n:o:vh' OPTION; do
   case "$OPTION" in
     n)
       ns="$OPTARG"
@@ -31,13 +105,17 @@ while getopts 'n:o:h' OPTION; do
       outdir="$OPTARG"
       ;;
 
+    v)
+      version_flag=1
+      ;;
+
     h)
-      echo "USAGE: spectrum-scale-driver-snap.sh [-n namespace] [-o output-dir] [-h]"
+      echo "USAGE: spectrum-scale-driver-snap.sh [-n namespace] [-o output-dir] [-v] [-h]"
       exit 0
       ;;
 
     ?)
-      echo "USAGE: spectrum-scale-driver-snap.sh [-n namespace] [-o output-dir] [-h]"
+      echo "USAGE: spectrum-scale-driver-snap.sh [-n namespace] [-o output-dir] [-v] [-h]"
       exit 1
       ;;
   esac
@@ -65,11 +143,17 @@ then
   exit 1
 fi
 
+if [[ $version_flag -eq 1 ]]
+then
+ find_versions $ns $cmd
+ exit 0
+fi
+
 operator=`$cmd get deployment --field-selector=metadata.name==ibm-spectrum-scale-csi-operator --namespace $ns  |grep -v NAME |awk '{print $1}'`
 if [[ "$operator" != "ibm-spectrum-scale-csi-operator" ]]; then
       echo "ibm-spectrum-scale-csi driver and operator is not running in namespace $ns. Please provide a valid namespace"
       exit 1
- fi
+fi
 
 time=$(date +"%m-%d-%Y-%T")
 logdir=${outdir%/}/ibm-spectrum-scale-csi-logs_$time
@@ -88,6 +172,7 @@ get_configmap=${logdir}/ibm-spectrum-scale-csi-configmap
 get_k8snodes=${logdir}/ibm-spectrum-scale-csi-k8snodes
 get_daemonset=${logdir}/ibm-spectrum-scale-csi-daemonsets
 describe_CSIScaleOperator=${logdir}/ibm-spectrum-scale-csi-describe-CSIScaleOperator
+get_version_images=${logdir}/ibm-spectrum-scale-csi-versions
 
 for statefulSetName in `$cmd -n $ns get StatefulSet --no-headers -l "app.kubernetes.io/name=ibm-spectrum-scale-csi-operator" |  awk '{print $1}'`; do
   echo "$klog StatefulSet/${statefulSetName}"
@@ -144,6 +229,8 @@ then
    echo "$get_scc_cmd"
    $get_scc_cmd > "${logdir}"/${PRODUCT_NAME}-scc.log 2>&1 || :
 fi
+
+find_versions $ns $cmd >> "$get_version_images" 2>&1 || :
 
 get_clusterinfo_cmd="$cmd cluster-info dump --namespaces kube-system --output-directory=$logdir"
 echo "$get_clusterinfo_cmd"
