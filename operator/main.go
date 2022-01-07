706725aded23871c043147b9215249ab93951eff
Amol Dabhade <43408487+amdabhad@users.noreply.github.com>
2022-01-07 14:05:02 +0530

    Added an OCP namespace in watch list (#64)
    Added namespace `openshift-controller-manager` in watch list of operator, to successfully create resources specific to OCP.

--- operator/main.go
+++ operator/main.go
@@ -30,6 +30,7 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
@@ -39,6 +40,8 @@ import (
 	securityv1 "github.com/openshift/api/security/v1"
 	//+kubebuilder:scaffold:imports
 )
+
+const OCPControllerNamespace = "openshift-controller-manager"
 
 var (
 	scheme   = runtime.NewScheme()
@@ -112,7 +115,7 @@ func main() {
 	flag.Parse()
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
-
+	namespaces := []string{watchNamespace, OCPControllerNamespace}
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:             scheme,
 		MetricsBindAddress: metricsAddr,
@@ -120,7 +123,7 @@ func main() {
 		//		HealthProbeBindAddress: probeAddr,
 		LeaderElection:   enableLeaderElection,
 		LeaderElectionID: "ibm-spectrum-scale-csi-operator",
-		Namespace:        watchNamespace, // namespaced-scope when the value is not an empty string
+		NewCache:         cache.MultiNamespacedCacheBuilder(namespaces),
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
