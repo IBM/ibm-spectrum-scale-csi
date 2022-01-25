2168bb489d494dbebe76ba760cac78415189a290
Madhu Thorat <madhu.punjabi@in.ibm.com>
2022-01-24 19:23:44 -0800

    Added checks for inodeLimit and volDirBasePath
    Signed-off-by: Madhu Thorat <madhu.punjabi@in.ibm.com>

--- driver/csiplugin/gpfs_util.go
+++ driver/csiplugin/gpfs_util.go
@@ -251,6 +251,13 @@ func getScaleVolumeOptions(volOptions map[string]string) (*scaleVolume, error) {
 	if fsTypeSpecified && isSCTypeSpecified {
 		return &scaleVolume{}, status.Error(codes.InvalidArgument, "The parameters \"type\" and \"filesetType\" are mutually exclusive")
 	}
+	if fsTypeSpecified && inodeLimSpecified {
+		return &scaleVolume{}, status.Error(codes.InvalidArgument, "The parameters \"type\" and \"inodeLimit\" are mutually exclusive")
+	}
+	if fsTypeSpecified && volDirPathSpecified {
+		return &scaleVolume{}, status.Error(codes.InvalidArgument, "The parameters \"type\" and \"volDirBasePath\" are mutually exclusive")
+	}
+ 
 	if fsTypeSpecified || isSCTypeSpecified {
 		scaleVol.IsFilesetBased = true
 	}
