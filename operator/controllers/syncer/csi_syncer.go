ce2552dfc4df2bee16ebe42a56f009c4b4dfa5ac
Nitish Kumar <81582694+nitishkumar4@users.noreply.github.com>
2022-06-30 10:23:58 +0530

    Fix for operator  going into error state and cso status as `unknown`.  (#719)
    * Update csi_node.go
    
    initializing Spec fields before mergo.Merge()
    
    Update csi_node.go
    
    * Update csi_node.go
    
    initializing Spec fields before mergo.Merge()
    
    Update csi_node.go
    
    * fix for imagepullsecrets and envVar
    
    * Update csi_node.go
    
    Signed-off-by: nitish-kumar4 <nitish.kumar4@ibm.com>

--- operator/controllers/syncer/csi_syncer.go
+++ operator/controllers/syncer/csi_syncer.go
@@ -237,6 +237,8 @@ func (s *csiControllerSyncer) SyncAttacherFn() error {
 	out.Spec.Template.ObjectMeta.Annotations = s.driver.GetAnnotations("", "")
 	out.Spec.Template.Spec.NodeSelector = s.driver.GetNodeSelectors(s.driver.Spec.AttacherNodeSelector)
 	//out.Spec.Template.ObjectMeta.Annotations = s.driver.GetAnnotations()
+	out.Spec.Template.Spec.Tolerations = []corev1.Toleration{}
+	out.Spec.Template.Spec.ImagePullSecrets = []corev1.LocalObjectReference{}
 
 	err := mergo.Merge(&out.Spec.Template.Spec, s.ensureAttacherPodSpec(secrets), mergo.WithTransformers(transformers.PodSpec))
 	if err != nil {
@@ -272,6 +274,8 @@ func (s *csiControllerSyncer) SyncProvisionerFn() error {
 	out.Spec.Template.ObjectMeta.Annotations = s.driver.GetAnnotations("", "")
 	out.Spec.Template.Spec.NodeSelector = s.driver.GetNodeSelectors(s.driver.Spec.ProvisionerNodeSelector)
 	//out.Spec.Template.ObjectMeta.Annotations = s.driver.GetAnnotations()
+	out.Spec.Template.Spec.Tolerations = []corev1.Toleration{}
+	out.Spec.Template.Spec.ImagePullSecrets = []corev1.LocalObjectReference{}
 
 	err := mergo.Merge(&out.Spec.Template.Spec, s.ensureProvisionerPodSpec(secrets), mergo.WithTransformers(transformers.PodSpec))
 	if err != nil {
@@ -307,6 +311,8 @@ func (s *csiControllerSyncer) SyncSnapshotterFn() error {
 	out.Spec.Template.ObjectMeta.Annotations = s.driver.GetAnnotations("", "")
 	out.Spec.Template.Spec.NodeSelector = s.driver.GetNodeSelectors(s.driver.Spec.SnapshotterNodeSelector)
 	//out.Spec.Template.ObjectMeta.Annotations = s.driver.GetAnnotations()
+	out.Spec.Template.Spec.Tolerations = []corev1.Toleration{}
+	out.Spec.Template.Spec.ImagePullSecrets = []corev1.LocalObjectReference{}
 
 	err := mergo.Merge(&out.Spec.Template.Spec, s.ensureSnapshotterPodSpec(secrets), mergo.WithTransformers(transformers.PodSpec))
 	if err != nil {
@@ -342,6 +348,8 @@ func (s *csiControllerSyncer) SyncResizerFn() error {
 	out.Spec.Template.ObjectMeta.Annotations = s.driver.GetAnnotations("", "")
 	out.Spec.Template.Spec.NodeSelector = s.driver.GetNodeSelectors(s.driver.Spec.ResizerNodeSelector)
 	//out.Spec.Template.ObjectMeta.Annotations = s.driver.GetAnnotations()
+	out.Spec.Template.Spec.Tolerations = []corev1.Toleration{}
+	out.Spec.Template.Spec.ImagePullSecrets = []corev1.LocalObjectReference{}
 
 	err := mergo.Merge(&out.Spec.Template.Spec, s.ensureResizerPodSpec(secrets), mergo.WithTransformers(transformers.PodSpec))
 	if err != nil {
