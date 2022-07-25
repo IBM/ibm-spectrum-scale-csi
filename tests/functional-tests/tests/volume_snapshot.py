a103de73c5e6a5c4e982805a3a03168bd99258ec
Nitish Kumar <81582694+nitishkumar4@users.noreply.github.com>
2022-07-25 12:19:31 +0530

    Update volume_snapshot.py

--- tests/functional-tests/tests/volume_snapshot.py
+++ tests/functional-tests/tests/volume_snapshot.py
@@ -523,8 +523,7 @@ def test_snapshot_dynamic_volume_cloning_1():
     value_clone_passed = {"clone_pvc": [{"access_modes": "ReadWriteMany", "storage": "1Gi"}, {
         "access_modes": "ReadWriteOnce", "storage": "1Gi"}]}
     snapshot_object.test_dynamic(value_sc, test_restore=True,
-                                 value_pvc=value_pvc, value_clone_passed=value_clone_passed)
-                                 
+                                 value_pvc=value_pvc, value_clone_passed=value_clone_passed)                             
 
 def test_driver_volume_snapshot_Independent_1_to_Dependent_1():
     value_sc = {"volBackendFs": data["localFs"]}
