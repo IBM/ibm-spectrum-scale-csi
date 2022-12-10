fe7fa89a19eb12104248fbef43de015f58250d41
Badri Nath Pathak <badri.pathak@ibm.com>
2022-12-11 03:19:25 +0530

    volume expand volume success case
    Signed-off-by: Badri Nath Pathak <badri.pathak@ibm.com>

--- driver/csiplugin/scale_test.go
+++ driver/csiplugin/scale_test.go
@@ -2,8 +2,10 @@ package scale_test
 
 import (
 	"fmt"
+	"strconv"
 
 	scale "github.com/IBM/ibm-spectrum-scale-csi/driver/csiplugin"
+	"github.com/IBM/ibm-spectrum-scale-csi/driver/csiplugin/connectors"
 	mock_scale "github.com/IBM/ibm-spectrum-scale-csi/driver/csiplugin/mock_scale"
 	"github.com/container-storage-interface/spec/lib/go/csi"
 	"golang.org/x/net/context"
@@ -21,6 +23,7 @@ var _ = Describe("Scale", func() {
 		mockConnectors *mock_connectors.MockSpectrumScaleConnector
 		mockScale      *mock_scale.MockScaleDriverInterface
 		// driver       *scale.ScaleDriver
+		fileset *connectors.Fileset_v2
 	)
 
 	Context("CTest", func() {
@@ -69,8 +72,41 @@ var _ = Describe("Scale", func() {
 				MgmtUsername: "csiadmin",
 				MgmtPassword: "adminuser",
 			}) */
+
 			mockScale.EXPECT().GetConnMap("18359298820404492091").Return(mockConnectors, true).AnyTimes()
-			mockConnectors.EXPECT().GetFilesystemName(gomock.Any()).Return("", nil).AnyTimes()
+			mockConnectors.EXPECT().GetFilesystemName(gomock.Any()).Return("fs1", nil).AnyTimes()
+			mockConnectors.EXPECT().CheckIfFilesetExist("fs1", gomock.Any()).Return(true, nil).AnyTimes()
+			mockConnectors.EXPECT().ListFilesetQuota("fs1", gomock.Any()).Return("2097152K", nil).AnyTimes()
+
+			fileset = &connectors.Fileset_v2{
+				AFM: connectors.AFM{AFMPrimaryID: "",
+					AFMMode:                      "",
+					AFMTarget:                    "",
+					AFMAsyncDelay:                0,
+					AFMDirLookupRefreshInterval:  0,
+					AFMDirOpenRefreshInterval:    0,
+					AFMExpirationTimeout:         0,
+					AFMFileLookupRefreshInterval: 0,
+					AFMNumFlushThreads:           0,
+					AFMParallelReadChunkSize:     0,
+					AFMParallelReadThreshold:     0,
+					AFMParallelWriteChunkSize:    0,
+					AFMParallelWriteThreshold:    0,
+					AFMPrefetchThreshold:         0,
+					AFMRPO:                       0,
+					AFMEnableAutoEviction:        false,
+					AFMShowHomeSnapshots:         false,
+				},
+
+				Config: connectors.FilesetConfig_v2{FilesetName: "", FilesystemName: "",
+					Path: "/ibm/fs1/pvc-80a0976b-e5a8-4a10-9f27-81aaec7436b7", InodeSpace: 2, MaxNumInodes: 100352, PermissionChangeMode: "chmodAndSetacl", Comment: "Fileset created by IBM Container Storage Interface driver", IamMode: "off", Oid: 4, Id: 2, Status: "Linked", ParentId: 0, Created: "2022-11-22 11:10:45,000", IsInodeSpaceOwner: true, InodeSpaceMask: 1536, SnapID: 0, RootInode: 1048579},
+				FilesetName: "pvc-80a0976b-e5a8-4a10-9f27-81aaec7436b7"}
+
+			mockConnectors.EXPECT().ListFileset("fs1", gomock.Any()).Return(*fileset, nil).AnyTimes()
+
+			opt := make(map[string]interface{})
+			opt[connectors.UserSpecifiedInodeLimit] = strconv.FormatUint(200000, 10)
+			mockConnectors.EXPECT().UpdateFileset("fs1", gomock.Any(), opt).Return(nil).AnyTimes()
 			/* err := mockScale.SetupScaleDriver("name", "vendorVersion", "nodeID")
 			if err != nil {
 				fmt.Errorf("Failed to initialize Scale CSI Driver: %v", err)
