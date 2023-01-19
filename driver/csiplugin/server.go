4e15b8fdce0dcc55c537cab4f5eeee4033634e56
badri-pathak <badri.pathak@ibm.com>
2023-01-19 02:16:09 -0800

    added function for updating csi.sock permission
    Signed-off-by: badri-pathak <badri.pathak@ibm.com>

--- driver/csiplugin/server.go
+++ driver/csiplugin/server.go
@@ -96,6 +96,10 @@ func (s *nonBlockingGRPCServer) serve(endpoint string, ids csi.IdentityServer, c
 	if err != nil {
 		glog.Fatalf("Failed to listen: %v", err)
 	}
+	// Change csi.sock file permsion to read and write only
+	if err := os.Chmod(addr, 0600); err != nil {
+		glog.Fatalf("Failed to modify csi.sock permission: %v", err)
+	}
 
 	server := grpc.NewServer(opts...)
 	s.server = server
