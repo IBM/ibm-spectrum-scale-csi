3ff06d25365915aff59c1b15e5ed3bc863cf0a65
badri-pathak <badri.pathak@ibm.com>
2024-05-02 07:10:18 -0700

    refine condition
    Signed-off-by: badri-pathak <badri.pathak@ibm.com>

--- driver/csiplugin/controllerserver.go
+++ driver/csiplugin/controllerserver.go
@@ -898,8 +898,6 @@ func (cs *ScaleControllerServer) setScaleVolume(ctx context.Context, req *csi.Cr
 
 	if scaleVol.IsFilesetBased && uint64(volSize) <= smallestVolSize {
 		scaleVol.VolSize = smallestVolSize
-	} else if scaleVol.IsFilesetBased && uint64(volSize) > smallestVolSize && uint64(volSize) <= maximumPVSize {
-		scaleVol.VolSize = uint64(volSize)
 	} else if scaleVol.IsFilesetBased && uint64(volSize) > maximumPVSize {
 		return nil, false, "", fmt.Errorf("failed to create volume, request volume size: [%v] is greater than the allowed PV max size: [%v]", uint64(volSize), maximumPVSize)
 	} else {
