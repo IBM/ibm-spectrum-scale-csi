bc83511a74bda6fb689ec06aa31a4a4c9e772896
badri-pathak <badri.pathak@ibm.com>
2024-06-04 00:55:31 -0700

    fixed review comments
    Signed-off-by: badri-pathak <badri.pathak@ibm.com>

--- driver/csiplugin/controllerserver.go
+++ driver/csiplugin/controllerserver.go
@@ -259,7 +259,7 @@ func (cs *ScaleControllerServer) setQuota(ctx context.Context, scVol *scaleVolum
 		var hardLimit, softLimit string
 		hardLimit = strconv.FormatUint(scVol.VolSize, 10)
 		if scVol.VolumeType == cacheVolume {
-			softLimit = strconv.FormatUint(uint64(math.Ceil(softQuotaPercent/float64(100)*float64(scVol.VolSize))), 10)
+			softLimit = strconv.FormatUint(uint64(math.Round(softQuotaPercent/float64(100)*float64(scVol.VolSize))), 10)
 		} else {
 			softLimit = hardLimit
 		}
@@ -3022,7 +3022,7 @@ func (cs *ScaleControllerServer) ControllerExpandVolume(ctx context.Context, req
 		var hardLimit, softLimit string
 		hardLimit = strconv.FormatUint(capacity, 10)
 		if volumeIDMembers.StorageClassType == STORAGECLASS_CACHE {
-			softLimit = strconv.FormatUint(uint64(math.Ceil(float64(capacity)*float64(softQuotaPercent)/float64(100))), 10)
+			softLimit = strconv.FormatUint(uint64(math.Round(float64(capacity)*float64(softQuotaPercent)/float64(100))), 10)
 		} else {
 			softLimit = hardLimit
 		}
