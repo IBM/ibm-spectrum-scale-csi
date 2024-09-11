7916d1e9cff421e476bee8ddc51bb78d4f47aadb
badri-pathak <badri.pathak@ibm.com>
2024-09-11 02:23:05 -0700

    signoff
    Signed-off-by: badri-pathak <badri.pathak@ibm.com>

--- operator/controllers/csiscaleoperator_controller.go
+++ operator/controllers/csiscaleoperator_controller.go
@@ -2400,7 +2400,7 @@ func validateVolNamePrefix(key string, value string, data map[string]string, inv
 		logger.Info("validateVolNamePrefix parsed :", "volNamePrefix", value)
 		data[key[11:]] = value
 	} else {
-		logger.Error(fmt.Errorf("the input volume name prefix is not right,the string value must be between [3 to 5] length and doesn't contains any empty char,  volNamePrefix : %v", value), "Volume Name Prefix Error")
+		logger.Error(fmt.Errorf("the input volume name prefix is not right,the string value must be between [3 to 5] length and doesn't contains any empty chars,  volNamePrefix : %v", value), "Volume Name Prefix Error")
 		invalidEnvValue[key] = value
 	}
 }
