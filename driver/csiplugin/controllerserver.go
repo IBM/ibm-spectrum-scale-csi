649699dc28387f7ce60cd00aeb3fb9e528e5711b
badri-pathak <badri.pathak@ibm.com>
2024-09-30 05:58:04 -0700

    fixed issue of not to create temp dir for ro and lu modes
    Signed-off-by: badri-pathak <badri.pathak@ibm.com>

--- driver/csiplugin/controllerserver.go
+++ driver/csiplugin/controllerserver.go
@@ -638,8 +638,8 @@ func (cs *ScaleControllerServer) createFilesetVol(ctx context.Context, scVol *sc
 			return "", status.Error(codes.Internal, err.Error())
 		}
 
-		if scVol.VolumeType == cacheVolume {
-			// Create cacheTempDirName inside the created fileset
+		// Create a cacheTempDir inside the fileset for all the cacheModes except ro and LU modes.
+		if scVol.VolumeType == cacheVolume && !(scVol.CacheMode == afmModeRO || scVol.CacheMode == afmModeLU) {
 			err = cs.createDirectory(ctx, scVol, volName, fmt.Sprintf("%s/%s", targetBasePath, connectors.CacheTempDirName))
 			if err != nil {
 				return "", status.Error(codes.Internal, err.Error())
