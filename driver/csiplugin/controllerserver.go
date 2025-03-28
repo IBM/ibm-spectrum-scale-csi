8e8086d474570d132ad3dd4734d39da99df375f5
Badri Nath Pathak <badri.pathak@ibm.com>
2025-03-28 18:24:23 +0530

    fixed issue of comments for cg independent fileset (#1312)
    Signed-off-by: badri-pathak <badri.pathak@ibm.com>

--- driver/csiplugin/controllerserver.go
+++ driver/csiplugin/controllerserver.go
@@ -292,7 +292,7 @@ func (cs *ScaleControllerServer) validateCG(ctx context.Context, scVol *scaleVol
 	loggerId := utils.GetLoggerId(ctx)
 	klog.V(4).Infof("[%s] Validate CG for volume [%v]", loggerId, scVol)
 
-	fsetlist, err := scVol.Connector.ListCSIIndependentFilesets(ctx, scVol.VolBackendFs, scVol.PVCName, scVol.Namespace)
+	fsetlist, err := scVol.Connector.ListCSIIndependentFilesets(ctx, scVol.VolBackendFs)
 	if err != nil {
 		return "", err
 	}
@@ -559,7 +559,12 @@ func (cs *ScaleControllerServer) createFilesetVol(ctx context.Context, scVol *sc
 			}
 		} else {
 			// This means fileset is not present, create it
-			opt[connectors.FilesetCommentKey] = fmt.Sprintf(connectors.FilesetCommentValue, scVol.PVCName, scVol.Namespace)
+			if isCGIndependentFset {
+				opt[connectors.FilesetCommentKey] = fmt.Sprintf(connectors.FilesetCommentValue, volName, scVol.Namespace)
+			} else {
+				opt[connectors.FilesetCommentKey] = fmt.Sprintf(connectors.FilesetCommentValue, scVol.PVCName, scVol.Namespace)
+			}
+
 			fseterr = scVol.Connector.CreateFileset(ctx, scVol.VolBackendFs, volName, opt)
 		}
 
