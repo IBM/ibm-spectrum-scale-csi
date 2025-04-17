ef0a5a2114046c2ca515d6c4b9e43e00a581893b
Deepak Ghuge <deeghuge@in.ibm.com>
2025-04-17 12:37:55 +0530

    ARM64 Support with 2.13.2 (#1325)
    * 2.13.2 version changes & ARM64 support

--- operator/controllers/config/constants.go
+++ operator/controllers/config/constants.go
@@ -71,8 +71,8 @@ const (
 	ENVKubeVersion  = "KUBE_VERSION"
 	ENVCGPrefix     = "CSI_CG_PREFIX"
 	ENVSymDirPath   = "SYMLINK_DIR_PATH"
-	DriverVersion   = "2.13.1"
-	OperatorVersion = "2.13.1"
+	DriverVersion   = "2.13.2"
+	OperatorVersion = "2.13.2"
 
 	// Number of replica pods for CSI Sidecar deployment
 	ReplicaCount = int32(2)
@@ -86,10 +86,12 @@ const (
 	PPC = "ppc64le"
 	// IBM zSystems machine architecture supported by IBM Storage Scale CSI.
 	IBMSystem390 = "s390x"
+	// 64-Bit ARM architecture supported by IBM Storage Scale CSI.
+	ARM64 = "arm64"
 
 	//  Default images for containers
 
-	CSIDriverPluginImage = "quay.io/ibm-spectrum-scale/ibm-spectrum-scale-csi-driver:v2.13.1"
+	CSIDriverPluginImage = "quay.io/ibm-spectrum-scale/ibm-spectrum-scale-csi-driver:v2.13.2"
 	//  registry.k8s.io/sig-storage/csi-node-driver-registrar:v2.11.1
 	CSINodeDriverRegistrarImage = "registry.k8s.io/sig-storage/csi-node-driver-registrar@sha256:e01facb9fb9cffaf52d0053bdb979fbd8c505c8e411939a6e026dd061a6b4fbe" // #nosec G101 false positive
 	//  registry.k8s.io/sig-storage/livenessprobe:v2.14.0
