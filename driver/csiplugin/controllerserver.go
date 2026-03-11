c1d8afd6ef3b6051d7310bf812d2027768c6fc4f
Hemalatha Gajendran <hemalatha.gajendran@ibm.com>
2026-03-11 14:38:57 +0530

    fix for shallowcopy CG
    Signed-off-by: Hemalatha Gajendran <hemalatha.gajendran@ibm.com>

--- driver/csiplugin/controllerserver.go
+++ driver/csiplugin/controllerserver.go
@@ -2833,13 +2833,7 @@ func (cs *ScaleControllerServer) DeleteVolume(newctx context.Context, req *csi.D
 					isPvcFromSnapshot = true
 					snapshotName = volPath[1]
 					if volumeIdMembers.StorageClassType == STORAGECLASS_ADVANCED {
-						_, origFileset, filesetFound :=strings.Cut(before, mountInfo.MountPoint)
-						if filesetFound {
-							 independentFset = strings.TrimSuffix(origFileset, "/")
-						} else {
-							klog.Errorf("[%s] Invalid volume path to delete shallow copy volume reference for advanced storage class", loggerId)
-							return nil, status.Error(codes.Internal, "invalid volume path to delete shallow copy volume reference for advanced storage class")
-						}
+						independentFset = strings.TrimSuffix(before, "/")
 					} else {
 						fileset := volPath[2]
 						independentFset = strings.Replace(fileset, "-data", "", 1)
